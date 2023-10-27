7807c4d72e9b83a4712bef53647261576e79ed30
paolosait <paolosait7@gmail.com>
2023-10-27 13:18:48 +0100

    Info Card

---
+++ src/lib/components/InfoCard.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    export let title: string;
+    export let text: string;
+    export let link: string | undefined = undefined;
+</script>
+
+<div class="bg-white rounded-xl p-4">
+    <div>
+        {title}
+    </div>
+    <div>
+        {text}
+    </div>
+    {#if link}
+    <div>
+         See more information <a class="hover:underline font-medium" href={link}>here</a>
+    </div>
+        
+    {/if}
+   
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+    import InfoCard from "$lib/components/InfoCard.svelte";
     import SectionHeader from "$lib/components/SectionHeader.svelte";
 </script>
 
@@ -19,4 +20,19 @@
 <SectionHeader title={"Services"} subtitle={"Design and development"} />
 <SectionHeader title={"About Us"} subtitle={"The people behind 2V3.studio"} />
 <SectionHeader title={"Portfolio"} subtitle={"Projects we've worked on"} />
+
+<div class="h-[700px] px-24">
+    <SectionHeader title={"Services"} subtitle={"Design and development"} />
+    <div class="flex flex-row gap-4">
+        <InfoCard title={"Website"} text={"We do lots"} />
+        <InfoCard title={"Mobile App"} text={"We do lots"} link={"/"} />
+    </div>
+    <div class="h-4"></div>
+    <InfoCard title={"Bespoke Projects"} text={"We do lots"} />
+    <SectionHeader
+        title={"About Us"}
+        subtitle={"The people behind 2V3.studio"}
+    />
+    <SectionHeader title={"Portfolio"} subtitle={"Projects we've worked on"} />
+</div>
 
