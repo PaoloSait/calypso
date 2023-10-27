0e68cea78c60b658515cd95448c8de87f4b7408a
paolosait <paolosait7@gmail.com>
2023-10-27 11:04:50 +0100

    Moved things into components

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,17 @@
+<div class=" flex flex-col px-8 py-4 items-start gap-6 bg-background-grey">
+        <div class="h-8">
+            <img class="object-scale-down h-8" src="/2V3.studio.png" alt="" />
+        </div>
+        <div class="flex flex-row space-x-4">
+            <a href="/">Home</a><span class="text-red-500">/</span><a href="/"
+                >About</a
+            ><span class="text-red-500">/</span><a href="/">FAQs</a><span
+                class="text-red-500">/</span
+            ><a href="/">Contact</a><span class="text-red-500">/</span><a
+                href="/">Website Development</a
+            ><span class="text-red-500">/</span><a href="/"
+                >Mobile App Development</a
+            ><span class="text-red-500">/</span><a href="/">Portfolio</a>
+        </div>
+        <div class="text-xs">© 2023 2V3.studio. ALL RIGHTS RESERVED</div>
+    </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,11 @@
-<div class="bg-blue-400">
+<div class="bg-blue-400 h-[700px]">
     <h1>Welcome to SvelteKit</h1>
     <h1>Welcome to SvelteKit</h1>
     <h1>Welcome to SvelteKit</h1>
     <h1>Welcome to SvelteKit</h1>
     <h1>Welcome to SvelteKit</h1>
     <h1>Welcome to SvelteKit</h1>
-    
+
     <p>
         Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
     </p>
