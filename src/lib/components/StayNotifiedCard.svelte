fa4b7d24dff1989d8fa56466a24ed41aaf6f8270
Tarun Koteeswaran <tarunkoteeswaran@gmail.com>
2023-10-27 21:44:24 +0100

    TK: Email input on home page

--- src/lib/components/StayNotifiedCard.svelte
+++ src/lib/components/StayNotifiedCard.svelte
@@ -1,14 +1,15 @@
+<script>
+    import EmailInput from "./forms/EmailInput.svelte";
+</script>
+
 <div class="bg-white rounded-xl flex-row flex gap-2 w-full overflow-hidden">
     <div class="flex flex-col p-4 gap-4 w-full">
-        <div class="text-3xl font-semibold">
-            Stay Notified
-        </div>
+        <div class="text-3xl font-semibold">Stay Notified</div>
         <div>
-            To keep up to date with 2V3.studio and the work we are doing, join our mailing list below!
+            To keep up to date with 2V3.studio and the work we are doing, join
+            our mailing list below!
         </div>
-        <div class="h-8 bg-green-200 rounded">
-
-        </div>
+        <EmailInput />
     </div>
     <div class="bg-pink-500 w-full">
         <!-- <img src="/PainAndTurmoil.png" alt=""> -->
---
+++ src/lib/components/forms/EmailInput.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+</script>
+
+<form class="flex flex-row rounded-l">
+    <input
+        class="bg-gray-200 appearance-none rounded-l py-2 px-4 text-black leading-tight focus:outline-none w-3/4"
+        name="email"
+        type="email"
+        placeholder="Email"
+        required={true}
+    />
+    <button class="text-white bg-black w-1/4 rounded-r"> Join </button>
+</form>
