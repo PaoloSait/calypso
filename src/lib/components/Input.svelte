ce12f7cf4c2dd304cd34fb824d1a4842eefe1364
Tarun Koteeswaran <tarunkoteeswaran@gmail.com>
2023-10-27 15:51:30 +0100

    TK: Contact page

---
+++ src/lib/components/Input.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+    // multiline
+    export let multiline: boolean = false;
+    // id of input
+    export let name: string = "";
+    // type of input
+    export let type: string;
+    // placeholder for input
+    export let placeholder: string;
+    // whether to bind to input value
+    export let boundValue: string | null = null;
+    // unbound input value
+    export let unboundValue: string = "";
+    // is input required
+    export let required: boolean = false;
+</script>
+
+{#if multiline}
+    <textarea
+        name="text"
+        autocomplete="off"
+        {placeholder}
+        class="bg-white dark:bg-gray-600 dark:text-white appearance-none min-h-[80] rounded w-full grow py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:border-purple-500 resize-none"
+    />
+{:else if boundValue != null}
+    <input
+        class="bg-white dark:bg-gray-600 dark:text-white appearance-none rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white dark:focus:bg-gray-600 focus:border-purple-500"
+        {name}
+        type="text"
+        {placeholder}
+        bind:value={boundValue}
+        {required}
+    />
+{:else}
+    <input
+        class="bg-white dark:bg-gray-600 dark:text-white appearance-none rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white dark:focus:bg-gray-600 focus:border-purple-500"
+        {name}
+        {type}
+        {placeholder}
+        {required}
+        value={unboundValue}
+    />
+{/if}
--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,20 +1,40 @@
-<div class={`background h-[700px] flex flex-col items-center`}>
-    <div class="flex flex-col items-center justify-between h-full w-1/2 pb-4 pt-72">
-        <div class="text-6xl font-bold text-center leading-tight">
-            SUPERCHARGE YOUR BRAND WITH THE WEB
-        </div>
-        <div class="flex flex-row gap-2">
-            See our portfolio for examples of our work <span
-                class="material-symbols-outlined"
-            >
-                arrow_forward
-            </span>
+<script>
+    import Input from "$lib/components/Input.svelte";
+</script>
+
+<div class={`background h-[700px]`}>
+    <div class="flex flex-col px-10 w-full pt-10">
+        <div class="text-4xl font-semibold mb-5">Contact</div>
+        <div class="flex flex-row w-full">
+            <div class="w-full flex flex-col">
+                <div>
+                    Please don’t hesitate to get in touch if you have any
+                    questions for us! Use the form to the right.
+                </div>
+                <div>or</div>
+                <div>
+                    Email us below with your project brief. Please be sure to
+                    include your estimated budget.
+                </div>
+                <div>team@2v3.studio</div>
+            </div>
+            <div class="w-40 border border-black"></div>
+            <div class="w-full flex flex-col gap-2">
+                <Input name="name" placeholder="Name" type="text" />
+                <Input name="email" placeholder="Email" type="email" />
+                <Input
+                    name="message"
+                    placeholder="Message"
+                    type="text"
+                    multiline={true}
+                />
+            </div>
         </div>
     </div>
 </div>
 
 <style>
     .background {
-        background: linear-gradient(124deg, #F2F2F2 14.76%, #EAEAEA 76.15%);
+        background: linear-gradient(124deg, #f2f2f2 14.76%, #eaeaea 76.15%);
     }
 </style>
