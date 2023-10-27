13bda40cb48e83d485f677a34bba9cf3723bd521
Tarun Koteeswaran <tarunkoteeswaran@gmail.com>
2023-10-27 13:04:29 +0100

    TK: Bit o text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,21 @@
 <script>
     import SectionHeader from "$lib/components/SectionHeader.svelte";
-
 </script>
-<div class="bg-blue-400 h-[700px]">
-    <SectionHeader title={"Services"} subtitle={"Design and development"} />
-    <SectionHeader title={"About Us"} subtitle={"The people behind 2V3.studio"} />
-    <SectionHeader title={"Portfolio"} subtitle={"Projects we've worked on"} />
+
+<div class="bg-blue-400 h-[700px] flex flex-col items-center">
+    <div class="flex flex-col items-center justify-center h-full w-1/2 gap-20">
+        <div class="text-6xl font-bold text-center leading-tight">
+            SUPERCHARGE YOUR BRAND WITH THE WEB
+        </div>
+        <div class="flex flex-row gap-2">
+            See our portfolio for examples of our work <span
+                class="material-symbols-outlined"
+            >
+                arrow_forward
+            </span>
+        </div>
+    </div>
 </div>
+<SectionHeader title={"Services"} subtitle={"Design and development"} />
+<SectionHeader title={"About Us"} subtitle={"The people behind 2V3.studio"} />
+<SectionHeader title={"Portfolio"} subtitle={"Projects we've worked on"} />
