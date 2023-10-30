3d7d3ed61fa51ee5e47babed0486a1c4f74d24f0
Tarun Koteeswaran <tarunkoteeswaran@gmail.com>
2023-10-30 19:54:36 +0000

    TK: Nicer FAQ

--- src/lib/components/contact/FAQCollapsible.svelte
+++ src/lib/components/contact/FAQCollapsible.svelte
@@ -1,23 +1,24 @@
 <script lang="ts">
     import { linear } from "svelte/easing";
-    import { fade } from "svelte/transition";
+    import { fade, slide } from "svelte/transition";
 
     let open = false;
     export let title: string;
     export let content: string;
 </script>
 
-<div class="flex flex-col gap-2">
+<div class="flex flex-col">
     <button class="flex flex-row items-center" on:click={() => (open = !open)}
         ><img
             src={open ? "/arrow_drop_down.svg" : "/arrow_right.svg"}
             alt="dropdown_arrow"
-            class="w-5 "
+            class="w-5"
         />
         <div class="text-xl">{title}</div>
     </button>
+    <div class="h-2" />
     {#if open}
-        <div class="flex flex-row text-base" transition:fade={{duration: 200}}>
+        <div class="flex flex-row text-lg" transition:slide={{}}>
             <div class="w-7" />
             {content}
         </div>
--- src/lib/components/contact/FAQSection.svelte
+++ src/lib/components/contact/FAQSection.svelte
@@ -6,7 +6,7 @@ import FaqCollapsible from "./FAQCollapsible.svelte";
 <div class="flex flex-col items-center pt-8 pb-16">
    <SectionHeader title="FAQs" subtitle="Things people ask us"/>
     <div class="h-8"></div>
-    <div class="flex flex-col items-start w-1/2 gap-8">
+    <div class="flex flex-col items-start w-1/2 gap-6">
         <FaqCollapsible
             title="How long does it take to build a website"
             content="It depends! Projects can take from a few days to a few months depending on the scale of the project, and the client's priorities"
@@ -14,15 +14,15 @@ import FaqCollapsible from "./FAQCollapsible.svelte";
         <FaqCollapsible
             title="Do I need two separate apps built for Android and iOS"
             content="No! Using Flutter we build Android and IOS apps from the same codebase, allowing more consistent UI design and cheaper
-            costs for our clients!"
+            costs for our clients."
         />
         <FaqCollapsible
             title="How much will it cost to have a website/app built"
-            content="Our website building service starts at £800, while our app building service starts at £1500. Please get in touch above for more precise quotes!"
+            content="Our website building service starts at £800, while our app building service starts at £1500. Please get in touch above for more precise quotes."
         />
         <FaqCollapsible
             title="Can you build a website that I’ve had designed already"
-            content="Of course! While we can design your website in-house, we are more than happy to bring an existing vision to life!"
+            content="Of course! While we can design your website in-house, we are more than happy to bring an existing vision to life."
         />
     </div>
 </div>
