1deb250f12535dfb6fdf010c669016867b6ca6f2
paolosait <paolosait7@gmail.com>
2023-11-01 17:03:08 +0000

    Fixed nav bar z hierachy mistake

--- src/lib/components/layout/NavHeader.svelte
+++ src/lib/components/layout/NavHeader.svelte
@@ -1,5 +1,5 @@
 <div
-    class="h-20 flex flex-row px-8 items-center gap-6 fixed top-0 right-0 left-0 bg-background-grey"
+    class="h-20 flex flex-row z-50 px-8 items-center gap-6 fixed top-0 right-0 left-0 bg-background-grey"
 >
     <a class="h-14" href="/">
         <img class="object-scale-down h-14" src="/2V3.studio.png" alt="" />
