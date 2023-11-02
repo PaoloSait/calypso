c7528c92fad65e35969658ee584ef89b024debc3
Tarun Koteeswaran <tarunkoteeswaran@gmail.com>
2023-11-02 16:37:18 +0000

    TK: Bios

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -2,7 +2,9 @@
     import DevHeadline from "$lib/components/layout/DevHeadline.svelte";
 </script>
 
-<DevHeadline background="linear-gradient(168deg, #CED9FF 10.21%, #E4CEFF 85.01%)">
+<DevHeadline
+    background="linear-gradient(168deg, #CED9FF 10.21%, #E4CEFF 85.01%)"
+>
     <div class="flex flex-col" slot="text">
         <div class="text-5xl font-semibold my-10">About Us</div>
         <div class="w-full flex flex-col gap-4 text-xl">
@@ -37,10 +39,11 @@
             <div class="font-semibold">MPhys Physics, Oxford University</div>
 
             <div class="text-lg">
-                Reliable? No. Always got your side? No. Will you snake you out
-                to go play tennis with some rando bastard? Sure bet you he will.
-                He will slave away making you an app because that is what he
-                does
+                I am a recent physics undergraduate from the University of
+                Oxford, who is passionate about bringing clients ideas to life.
+                Having gained valuable experience working at tech startups, I
+                have a firm understanding of how to build resilient websites
+                that scale with user demands.
             </div>
         </div>
     </div>
@@ -52,11 +55,11 @@
                 MMath Mathematics, Oxford University
             </div>
             <div class="text-lg">
-                We are Tarun and Paolo, two recent graduates from Oxford
-                University. We are web and mobile developers with a passion for
-                design and coding. We love working with new clients to
-                understand their ideas and help bring them to life with the
-                power of the internet.
+                I hold a degree in Mathematics from the University of Oxford and
+                possess a keen eye for design. During my degree, I
+                undertook various coding projects, giving me a wide range of
+                computing skills. During my free time, I enjoy making music and
+                crafting visual media!
             </div>
         </div>
         <div class="rounded-full bg-red-400 w-40 h-40 shrink-0" />
