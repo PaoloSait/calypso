5b865e7862616d409de1b5081263de1988b482f3
paolosait <paolosait7@gmail.com>
2023-11-02 11:52:31 +0000

    Made footer work on mobile

--- src/lib/components/layout/Footer.svelte
+++ src/lib/components/layout/Footer.svelte
@@ -7,16 +7,17 @@
     <div class="h-8">
         <img class="object-scale-down h-8" src="/2V3.studio.png" alt="" />
     </div>
-    <div class="flex flex-row space-x-4">
-        <a href="/">Home</a><span class="text-red-500">/</span><a href="/about"
+    <span>
+        <a href="/">Home</a><span class="text-red-500 px-2">/</span><a href="/about"
             >About</a
-        ><span class="text-red-500">/</span><a href="/contact">FAQs</a><span
-            class="text-red-500">/</span
-        ><a href="/contact">Contact</a><span class="text-red-500">/</span><a
-            href="/website">Website Development</a
-        ><span class="text-red-500">/</span><a href="/app"
-            >Mobile App Development</a
-        ><span class="text-red-500">/</span>
+        ><span class="text-red-500 px-2">/</span><a href="/contact">FAQs</a
+        ><span class="text-red-500 px-2">/</span><a href="/contact">Contact</a><span
+            class="text-red-500 px-2">/</span
+        ><a href="/website">Website Development</a><span class="text-red-500 px-2"
+            >/</span
+        ><a href="/app">Mobile App Development</a><span class="text-red-500 px-2"
+            >/</span
+        >
         {#if $page.url.pathname == "/"}
             <div use:scrollTo={{ ref: "Portfolio", offset: -100 }}>
                 Portfolio
@@ -24,6 +25,6 @@
         {:else}
             <a href="/">Portfolio</a>
         {/if}
-    </div>
+    </span>
     <div class="text-xs">© 2023 2V3.studio. ALL RIGHTS RESERVED</div>
 </div>
