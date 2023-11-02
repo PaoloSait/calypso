b33b9643883b5d0d79b7d7d9f8b565249c7ef709
Tarun Koteeswaran <tarunkoteeswaran@gmail.com>
2023-11-02 13:13:18 +0000

    TK: Header

--- src/lib/components/layout/NavHeader.svelte
+++ src/lib/components/layout/NavHeader.svelte
@@ -1,10 +1,9 @@
 <div
-    class="h-20 flex flex-row z-50 px-8 items-center gap-6 fixed top-0 right-0 left-0 bg-background-grey"
+    class="h-20 flex flex-row z-50 md:px-8 px-4 items-center md:gap-6 gap-3 fixed top-0 right-0 left-0 bg-background-grey text-sm md:text-base"
 >
     <a class="h-14" href="/">
         <img class="object-scale-down h-14" src="/2V3.studio.png" alt="" />
     </a>
-
     <div class="w-full" />
     <a class="hover:font-medium" href="/">Menu</a>
     <a class="hover:font-medium" href="/about">About</a>
