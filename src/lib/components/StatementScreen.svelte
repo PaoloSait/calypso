2bebb8479af0581274af20f14a19a9b0e7afc92e
Tarun Koteeswaran <tarunkoteeswaran@gmail.com>
2023-11-02 13:15:25 +0000

    TK: Fixed dodgy calc

--- src/lib/components/StatementScreen.svelte
+++ src/lib/components/StatementScreen.svelte
@@ -2,11 +2,13 @@
     import { scrollTo } from "svelte-scrolling";
 </script>
 
-<div class="background flex flex-col items-center h-[calc(100vh_-_80px)]">
+<div class="background flex flex-col items-center h-[calc(100vh_-_5rem)]">
     <div
         class="flex flex-col items-center justify-between h-full w-full pb-8 pt-52"
     >
-        <div class="lg:text-7xl md:text-6xl sm:text-5xl text-4xl font-bold text-center leading-tight animate-[fadeIn_2s_ease-in-out] w-3/4 sm:w-1/2">
+        <div
+            class="lg:text-7xl md:text-6xl sm:text-5xl text-4xl font-bold text-center leading-tight animate-[fadeIn_2s_ease-in-out] w-3/4 sm:w-1/2"
+        >
             SUPERCHARGE YOUR BRAND WITH THE WEB
         </div>
         <div class="flex flex-row gap-2 animate-[fadeIn_3s_ease-in-out]">
--- src/lib/components/layout/DevHeadline.svelte
+++ src/lib/components/layout/DevHeadline.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div
-    class="flex flex-row w-full py-10 px-28 background justify-between h-[calc(80vh_-_80px)]"
+    class="flex flex-row w-full py-10 px-28 background justify-between h-[calc(80vh_-_5rem)]"
     style:background
 >
     <slot name="text" />
