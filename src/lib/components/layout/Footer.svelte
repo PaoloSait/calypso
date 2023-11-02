5a53ff7a46a481c9a500127715deb35b511f0dd9
paolosait <paolosait7@gmail.com>
2023-11-02 14:00:48 +0000

    Fixed footer overrun

--- src/lib/components/layout/Footer.svelte
+++ src/lib/components/layout/Footer.svelte
@@ -3,27 +3,29 @@
     import { scrollTo } from "svelte-scrolling";
 </script>
 
-<div class=" flex flex-col px-8 py-4 items-start gap-6 bg-background-grey">
+<div class=" flex flex-col mx-8 my-4 items-start gap-6">
     <div class="h-8">
         <img class="object-scale-down h-8" src="/2V3.studio.png" alt="" />
     </div>
-    <span>
-        <a href="/">Home</a><span class="text-red-500 px-2">/</span><a href="/about"
-            >About</a
-        ><span class="text-red-500 px-2">/</span><a href="/contact">FAQs</a
-        ><span class="text-red-500 px-2">/</span><a href="/contact">Contact</a><span
-            class="text-red-500 px-2">/</span
-        ><a href="/website">Website Development</a><span class="text-red-500 px-2"
-            >/</span
-        ><a href="/app">Mobile App Development</a><span class="text-red-500 px-2"
-            >/</span
-        >
+    <span class="w-full">
+        <a class="inline-block" href="/">Home</a>
+        <span class="text-red-500 px-2 inline-block">/</span>
+        <a class="inline-block" href="/about">About</a>
+        <span class="text-red-500 px-2 inline-block">/</span>
+        <a class="inline-block" href="/contact">FAQs</a>
+        <span class="text-red-500 px-2 inline-block">/</span>
+        <a class="inline-block" href="/contact">Contact</a>
+        <span class="text-red-500 px-2 inline-block">/</span>
+        <a class="inline-block" href="/website">Website Development</a>
+        <span class="text-red-500 px-2 inline-block">/</span>
+        <a class="inline-block" href="/app">Mobile App Development</a>
+        <span class="text-red-500 px-2 inline-block">/</span>
         {#if $page.url.pathname == "/"}
-            <div use:scrollTo={{ ref: "Portfolio", offset: -100 }}>
+            <div class="inline-block" use:scrollTo={{ ref: "Portfolio", offset: -100 }}>
                 Portfolio
             </div>
         {:else}
-            <a href="/">Portfolio</a>
+            <a class="inline-block" href="/">Portfolio</a>
         {/if}
     </span>
     <div class="text-xs">© 2023 2V3.studio. ALL RIGHTS RESERVED</div>
