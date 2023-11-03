73551559cf714c4e9571a369f67db0eae0d8080f
Tarun Koteeswaran <tarunkoteeswaran@gmail.com>
2023-11-03 15:56:14 +0000

    New Nav

--- src/lib/components/layout/NavHeader.svelte
+++ src/lib/components/layout/NavHeader.svelte
@@ -1,14 +1,30 @@
-<div
-    class="h-20 flex flex-row w-full justify-between z-50 md:px-8 px-4 items-center fixed top-0 right-0 left-0 bg-background-grey text-xs md:text-base"
->
-    <a class="h-3 sm:h-5 md:h-8 lg:h-10" href="/">
-        <img class="object-contain h-3 sm:h-5 md:h-8 lg:h-10" src="/2V3.studio.png" alt="" />
-    </a>
+<script>
+    import {
+        Navbar,
+        NavBrand,
+        NavLi,
+        NavUl,
+        NavHamburger,
+    } from "flowbite-svelte";
+</script>
 
-    <div class="flex flex-row md:gap-6 gap-3">
-        <a class="hover:font-medium" href="/website">Website Dev</a>
-        <a class="hover:font-medium" href="/app">App Dev</a>
-        <a class="hover:font-medium" href="/about">About</a>
-        <a class="hover:font-medium" href="/contact">Contact</a>
-    </div>
-</div>
+<Navbar let:NavContainer>
+    <NavContainer
+        class="h-20 flex flex-row w-full justify-between z-50 md:px-8 px-4 items-center fixed top-0 right-0 left-0 bg-background-grey text-xs md:text-base"
+    >
+        <NavBrand href="/" class="h-3 sm:h-5 md:h-8 lg:h-10">
+            <img
+                class="object-contain h-3 sm:h-5 md:h-8 lg:h-10"
+                src="/2V3.studio.png"
+                alt=""
+            />
+        </NavBrand>
+        <NavHamburger />
+        <NavUl>
+            <NavLi href="/website">Website Dev</NavLi>
+            <NavLi href="/app">App Dev</NavLi>
+            <NavLi href="/about">About</NavLi>
+            <NavLi href="/contact">Contact</NavLi>
+        </NavUl>
+    </NavContainer>
+</Navbar>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,19 @@ export default {
   theme: {
     extend: {
       colors: {
-        'background-grey': '#F8F8F8',
+        // flowbite-svelte
+        primary: {
+          50: '#FFF5F2',
+          100: '#FFF1EE',
+          200: '#FFE4DE',
+          300: '#FFD5CC',
+          400: '#FFBCAD',
+          500: '#FE795D',
+          600: '#EF562F',
+          700: '#EB4F27',
+          800: '#CC4522',
+          900: '#A5371B'
+        }
       },
       fontFamily: {
         display: ["Libre Franklin", "sans-serif"],
@@ -18,6 +30,7 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [require('flowbite/plugin')],
+  darkMode: 'class',
 }
 
