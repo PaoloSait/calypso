de577c1ae4b34f2af3a4ce7f78574e5b197db4d9
Tarun Koteeswaran <tarunkoteeswaran@gmail.com>
2023-11-03 17:48:50 +0000

    TK: Got rid of borders

--- src/lib/components/forms/EmailInput.svelte
+++ src/lib/components/forms/EmailInput.svelte
@@ -4,7 +4,7 @@
 
 <form class="flex flex-row rounded-l-lg" method="POST" action="/" use:enhance>
     <input
-        class="bg-gray-200 appearance-none rounded-l-lg py-2 px-4 text-black leading-tight focus:outline-none w-3/4"
+        class="bg-gray-200 appearance-none rounded-l-lg py-2 px-4 text-black leading-tight focus:outline-none w-3/4 border-none focus:border-none"
         name="email"
         type="email"
         placeholder="Email"
