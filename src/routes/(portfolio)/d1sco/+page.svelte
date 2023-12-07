bc6892e72aff2c8c48efe29ab7629617e547654a
Tarun Koteeswaran <tarunkoteeswaran@gmail.com>
2023-12-07 15:16:33 +0000

    TK: Updated wording for d1sco page

--- src/routes/(portfolio)/d1sco/+page.svelte
+++ src/routes/(portfolio)/d1sco/+page.svelte
@@ -5,27 +5,26 @@
 </p>
 <p>
     
-        Note: <em>D1SCO is in a beta-test stage so to join you will need a sign-up
-        code. If you would like a code please email us at contact@2v3.studio.
+        Note: <em>D1SCO is currently in Beta! To join request a sign-up code by emailing
+        us at contact@2v3.studio.
     </em>
 </p>
 <p>
-    D1SCO is a platform for communities centred around a love for music. It's a
-    place where people can share, discuss and discover music.
+    D1SCO is a platform for communities centred around music,
+    allowing users to share, discuss and discover the music they love.
 </p>
 <p>
-    It is a project that we ourselves at 2V3.studio dreamt up and decided to
-    turn into reality. A priority for this project was to create a working
-    version of the idea in a short period of time. From outlining the
-    specification to having initial users on the platform and testing it took
-    only 3 weeks.
+    This project was one which we at 2v3.studio came up with ourselves.
+    Our main priority when building the app was to create a working
+    version in a short period of time. It only took us 3 weeks from outlining the
+    idea to having initial users on the platform!
 </p>
 <img src="D1scoPreview.png" alt="Screenshot of D1sco website">
 <h3>Design</h3>
 <p>
-    We followed our usual design process of first making it very clear what we
-    wanted the prototype to do. This is something we usually do over video calls
-    with clients. The specification of the platform was:
+    We followed our standard design process, first establishing the core functionality of the prototype.
+    This is something we usually do over video calls
+    with clients. Our specification for the D1SCO platform was:
     <li>Log-in and sign-up system</li>
     <li>The ability to search for any song, album or artist</li>
     <li>A way of sharing a song/album/artist with a text caption</li>
@@ -35,40 +34,32 @@
     Once we'd finalised our specification, we went on to wireframe the web app.
 </p>
 <blockquote>
-    A wireframe is a visual representation or skeletal outline of a digital
+    A wireframe is a skeletal outline of a digital
     interface or design, illustrating the layout and basic elements without
-    detailed design elements or graphics.
+    detailed design elements.
 </blockquote>
-<p>
-    Instead of desigining the entire UI, we desigined the rough style we wanted
-    a post to follow and then let the rest of the design follow ad hoc during
-    development. Although we wouldn't usually do this, it enabled us to start
-    development sooner and make progress more quickly.
-</p>
 <h3>Develop</h3>
 <p>
-    To develop the web app, we used a tech stach that we have lots of experience
-    with. This is comprised of:
-    <li>Svelte / SvelteKit for frontend</li>
-    <li>Vercel for hosting</li>
-    <li>Firebase for backend</li>
+    To develop the web app, we used the following tech stack:
+    <li>SvelteKit Frontend</li>
+    <li>Firebase Backend</li>
+    <li>Vercel Hosting</li>
 </p>
 <p>
-    Instead of building our own catalog of all songs and artists, we used the <a
+    To provide a library of songs, albums and artists that users could share within the app, we used the <a
         href="https://developer.spotify.com/documentation/web-api"
         >Spotify Api</a
-    >. This allowed us to enable search within the web app, a key part of our
-    specification.
+    >. By connecting our service to Spotify, we were able to introduce full-text song search, and 
+    display an artists most streamed songs.
 </p>
 <h3>Release</h3>
 <p>
     We decided to host D1SCO through Vercel using a testing url <a href="/"
         >www.D1SCO.vercel.app</a
-    >. This allowed us to publish the website live without having to buy a
-    domain so that we could get user feedback before committing to a name and
+    >. This allowed us to publish the website live and get user feedback before committing to a name and
     brand.
 </p>
 <p>
-    When we work with clients, we will host their website on a testing url so
-    they can see the website before it gets published live to their domain.
+    When we work with clients, we will host their website on a testing url so that
+    they can review the website before it gets published live to their domain.
 </p>
