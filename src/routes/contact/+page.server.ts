b4421056ec9302220937c402809d85ec2cdac7a0
paolosait <paolosait7@gmail.com>
2024-01-16 16:21:09 +0000

    Tidied up contact server code

--- src/routes/contact/+page.server.ts
+++ src/routes/contact/+page.server.ts
@@ -1,16 +1,15 @@
 import { RESEND_API_KEY } from '$env/static/private';
 import { Resend } from 'resend';
 
-
 /** @type {import('./$types').Actions} */
 export const actions = {
     default: async ({ request }) => {
-        console.log("Tarun");
         const data = await request.formData();
         const email = data.get('email') as string;
         const message = data.get('message') as string;
         const name = data.get('name') as string;
-        const checked = data.get('checkbox') as string; const resend = new Resend(RESEND_API_KEY);
+        const checked = data.get('checkbox') as string; 
+        const resend = new Resend(RESEND_API_KEY);
         if (checked == 'on') {
             try {
                 const data = await resend.emails.send({
