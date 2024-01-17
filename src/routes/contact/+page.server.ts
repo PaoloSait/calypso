e8ef2b9129bd8f932cd18c482e5e07f7ccf1fdb5
paolosait <paolosait7@gmail.com>
2024-01-17 12:57:58 +0000

    Updated contact form email format

--- src/routes/contact/+page.server.ts
+++ src/routes/contact/+page.server.ts
@@ -8,16 +8,17 @@ export const actions = {
         const email = data.get('email') as string;
         const message = data.get('message') as string;
         const name = data.get('name') as string;
-        const checked = data.get('checkbox') as string; 
+        const checked = data.get('checkbox') as string;
         const resend = new Resend(RESEND_API_KEY);
         if (checked == 'on') {
             try {
                 const data = await resend.emails.send({
-                    from: `onboarding@resend.dev`,
+                    from: `2V3.studio <msg@2v3.studio>`,
                     to: ['tarunkoteeswaran@gmail.com', 'paolosait7@gmail.com'],
-                    subject: `Dev Request: ${name}`,
-                    html: `<div> From ${email}</div>
-                <div>${message}</div>`
+                    subject: `Message from ${name}`,
+                    html: `<h2>One new message from ${name}</h2><p>"${message}"</p>
+                    <p>Email to reply to: ${email}</p>
+                `
                 });
                 console.log(data);
             } catch (error) {
@@ -25,6 +26,4 @@ export const actions = {
             }
         }
     }
-
-
 }
