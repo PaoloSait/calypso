3afb13d1d0a43d38768f263004b6c8307eb05b70
paolosait <paolosait7@gmail.com>
2024-05-15 17:49:25 +0800

    Updated FAQ section

--- src/lib/components/contact/FAQCollapsible.svelte
+++ src/lib/components/contact/FAQCollapsible.svelte
@@ -3,24 +3,23 @@
     import { fade, slide } from "svelte/transition";
 
     let open = false;
-    export let title: string;
-    export let content: string;
 </script>
 
 <div class="flex flex-col">
-    <button class="flex flex-row items-start" on:click={() => (open = !open)}
-        ><img
+    <button class="flex flex-row items-center gap-4" on:click={() => (open = !open)}
+        >
+        <div class="w-20 h-3 bg-background-black md:block hidden"></div>
+        <img
             src="/arrow_right.svg"
             alt="dropdown_arrow"
-            class={`w-5 mt-[2px] lg:mt-1 ${open ? "rotate-90" : ""} transition-all`}
+            class={`w-7 mt-[2px] ${open ? "rotate-90" : ""} transition-all md:hidden block`}
         />
-        <div class="md:text-xl text-start ">{title}</div>
+        <div class="md:text-2xl text-start "><slot name="title"></slot></div>
     </button>
     <div class="h-2" />
     {#if open}
-        <div class="flex flex-row text-sm sm:text-base" transition:slide={{}}>
-            <div class="w-7" />
-            {content}
+        <div class="flex flex-row text-sm sm:text-base md:text-lg md:ml-24 ml-10" transition:slide={{}}>
+            <slot name="content"></slot>
         </div>
     {/if}
 </div>
