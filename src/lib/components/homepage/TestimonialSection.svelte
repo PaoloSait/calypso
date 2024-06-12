0a2acadbb80c34ad943df8b0b7e2acab4a9c7b8c
paolosait <paolosait7@gmail.com>
2024-06-12 17:21:33 +0100

    Testimonial

--- src/lib/components/homepage/TestimonialSection.svelte
+++ src/lib/components/homepage/TestimonialSection.svelte
@@ -2,21 +2,27 @@
     import H2 from "../text/H2.svelte";
 </script>
 
-<div class="flex flex-col gap-6 items-center">
+<div class="flex flex-col items-center">
     <H2>Hear from our customers</H2>
-    <div class="md:text-xl sm:text-lg text-base text-center max-w-2xl">
-        <span class="text-4xl text-highlight-purple">"</span>
-        2V3.studio is a full stack agency, Design through development of websites
-        and mobile applications is our expertise<span
-            class="text-4xl text-highlight-purple">"</span
+    <div class="flex flex-row">
+        <img src="/start-quote.png" class="w-20 h-20 self-start" alt="start quote">
+        <div
+            class="md:text-xl sm:text-lg text-base leading-8 text-center max-w-2xl mx-2 my-8"
         >
+            5/5 - Highly Enjoyable Experience. Paolo and Tarun have been great
+            to work with from the start. Their clear communication and
+            explanations made the process easy to understand. They will take
+            time to answer questions thoroughly and also help bounce ideas
+            around for the app.
+        </div>
+        <img src="/end-quote.png" class="w-20 h-20 self-end" alt="start quote">
     </div>
 
     <div class="flex flex-row gap-5 items-center">
-        <div class="md:w-12 md:h-12 h-8 w-8 rounded-full bg-blue-300"></div>
+        <img src="/chums/chums-logo-white-on-orange.jpeg" class="md:w-12 md:h-12 h-8 w-8 rounded-full" alt="chums logo"/>
         <div class="flex flex-col">
             <div class="md:text-lg text-base font-semibold">Alice Thorne</div>
-            <div class="md:text-base text-xs">Cofounder of Travel Chums</div>
+            <div class="md:text-base text-xs">Cofounder of TRAVEL CHUMS LTD</div>
         </div>
     </div>
 </div>
